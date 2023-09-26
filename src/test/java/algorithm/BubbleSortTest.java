e10cf7b5c4fd00a96f2d52437d8c1bf71f5da973
wutianwen <wutianwenzy@foxmail.com>
2023-09-26 13:03:37 +0800

    测试文件

--- src/test/java/algorithm/BubbleSortTest.java
+++ src/test/java/algorithm/BubbleSortTest.java
@@ -6,7 +6,7 @@ public class BubbleSortTest {
 
     @org.junit.Test
     public void bubbleSort() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {3, 4, 1, 2, 10, 6, 8, 5, 9, 7};
         BubbleSort bubbleSort = new BubbleSort();
         bubbleSort.bubbleSort(arr);
         assertArrayEquals(arr, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
